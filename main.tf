60d05030c8586ce795659f00c0fefc8bf9d0f9d4
BenH <ben.higginbottom@gmail.com>
2017-09-05 13:50:44 +0100

    First Commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region                  = "${var.aws_region}"
+  shared_credentials_file = "/home/ben/.aws/credentials"
+}
+
+
+module "scan" {
+  source = "github.com/BenHigginbottom/tf_modules//scan"
+}
+
+module "latestAMI" {
+  source = "github.com/BenHigginbottom/tf_modules//latestAMI"
+  
+}
+
+module "ec2" {
+  source = "github.com/BenHigginbottom/tf_modules//ec2dist"
+  INSTANCES = "2"
+  AMI = "${module.latestAMI.ec2linuxd}"
+  INSTTYPE = "t2.micro"
+  VPCSG = "${module.scan.security_group}"
+  AZ = "${module.scan.names}"
+  SUBNETS = "${module.scan.subnets}"
+}
+
+module "ELB" {
+  source = "github.com/BenHigginbottom/tf_modules//ELB"
+  NAME = "Bens-Test-ELB"
+  PORT = "80"
+  DESTPORT = "443"
+  INSTANCES = ["${module.ec2.ids}"]
+}
